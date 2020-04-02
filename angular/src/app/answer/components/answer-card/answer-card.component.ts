4e6078c617eaf8b9989f7441d68c84122392ea18
wmc <1006004868@qq.com>
2020-04-02 21:43:07 +0800

    wmc

--- angular/src/app/answer/components/answer-card/answer-card.component.ts
+++ angular/src/app/answer/components/answer-card/answer-card.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MyCard } from 'src/app/my';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
 export interface QuestionCard {
   _id: string,
   questioner: string,
@@ -16,17 +18,18 @@ export interface QuestionCard {
 })
 export class AnswerCardComponent implements OnInit {
   @Input() card: QuestionCard
-  constructor(private http: HttpClient, private router: Router) { }
-  name$
+  constructor(private cookies: CookieService, private http: HttpClient, private router: Router) { }
+  my$: Observable<MyCard>
   at: string
+  _id
   handleClick() {
     this.router.navigate(["/answer/detail"], { queryParams: { _id: `${this.card._id}` } })
   }
 
 
   ngOnInit() {
-
-    this.name$ = this.http.get<MyCard>(`http://101.37.119.148:3000/users/${this.card.questioner}`)
+    this._id = this.cookies.get("_id");
+    this.my$ = this.http.get<MyCard>(`http://101.37.119.148:3000/users/${this._id}`)
   }
 
 }
